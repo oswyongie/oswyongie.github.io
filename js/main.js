5badd381cc14e62bfce27d69bac9f6effcb4d579
oswyongie <yunindanadia16@gmail.com>
2024-03-08 09:07:56 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,7 @@
 // Toggle & Responsive Navigation
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const navLists = document.querySelector("nav");
-  
-    burger.addEventListener("click", () => {
-      // Toggle nav list and burger class
-      navLists.classList.toggle("nav-active");
-      burger.classList.toggle("toggle-burger");
-    });
-  };
-  
-  navSlide();
-  
-  // Clear form before unload
-  window.onbeforeunload = () => {
-    for (const form of document.getElementsByTagName("form")) {
-      form.reset();
-    }
-  };
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+});
